eae1e0f2692f8b451a46f1307b72e6fb93000270
Vitoria de Sousa Oliveira <vitoriadesousaoliveira@outlook.com>
2025-10-07 00:30:20 +0000

    feat: statement

---
+++ tarefas/Operacoes.cs
@@ -0,0 +1,23 @@
+public class Operacoes
+{
+    public int Criar(Tarefa tarefa)
+    {
+        return 0;
+    }
+    public Tarefa Buscar(int id)
+    {
+        return null;
+    }
+    public List<Tarefa> Listar()
+    {
+        return Array.Empty<Tarefa>();
+    }
+    public void Alterar(Tarefa tarefa)
+    {
+
+    }
+    public void Excluir(int id)
+    {
+        
+    }
+}
